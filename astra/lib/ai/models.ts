dcfba8e69729214f856984560ed250ee04b348ea
Will Yang <will.yang@homepartners.tech>
2025-02-19 14:51:29 +0800

    feat: add frontend

---
+++ astra/lib/ai/models.ts
@@ -0,0 +1,53 @@
+import { openai } from '@ai-sdk/openai';
+import { fireworks } from '@ai-sdk/fireworks';
+import { deepseek } from '@ai-sdk/deepseek';
+
+import {
+  customProvider,
+  extractReasoningMiddleware,
+  wrapLanguageModel,
+} from 'ai';
+
+export const DEFAULT_CHAT_MODEL: string = 'chat-model-reasoning';
+
+export const myProvider = customProvider({
+  languageModels: {
+    'chat-model-small': openai('gpt-4o-mini'),
+    'chat-model-large': openai('gpt-4o'),
+    'chat-model-reasoning-fireworks': wrapLanguageModel({
+      model: fireworks('accounts/fireworks/models/deepseek-r1'),
+      middleware: extractReasoningMiddleware({ tagName: 'think' }),
+    }),
+    'chat-model-reasoning': deepseek('deepseek-reasoner'),
+    'title-model': deepseek('deepseek-chat'),
+    'block-model': openai('gpt-4o-mini'),
+  },
+  imageModels: {
+    'small-model': openai.image('dall-e-2'),
+    'large-model': openai.image('dall-e-3'),
+  },
+});
+
+interface ChatModel {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export const chatModels: Array<ChatModel> = [
+  {
+    id: 'chat-model-small',
+    name: 'Small model',
+    description: 'Small model for fast, lightweight tasks',
+  },
+  {
+    id: 'chat-model-large',
+    name: 'Large model',
+    description: 'Large model for complex, multi-step tasks',
+  },
+  {
+    id: 'chat-model-reasoning',
+    name: 'Reasoning model',
+    description: 'Uses advanced reasoning',
+  },
+];
